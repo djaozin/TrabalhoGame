e730d00150cd4c3201ec3bd9f205fc6abec912cb
Marconex_ <161333902+Ghxxt1@users.noreply.github.com>
2024-10-22 11:05:16 -0300

    Alteração 'teste'
    Adicionado sistema de time.sleep

--- game.py
+++ game.py
@@ -2,13 +2,19 @@ import os
 import time
 import random
 os.system('cls')
+
 print('''----------A Lenda de Eldoria II----------
 ---Sua aventura está prestes a começar---''')
 
 nome = input('Nome do personagem: ')
+
 hp = 8
 atk = 2
 df = 1
 esq = 1
 exp = 0
+
+print(f'Seja bem-vindo, {nome}. Você está entrando na caverna... ')
+print('Carregando...')
+time.sleep(1)
 
